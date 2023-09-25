6407b98ffdfb20bb617ef9425484149dc111751a
Anjali167-prog <69163841+Anjali167-prog@users.noreply.github.com>
2023-09-25 18:35:43 +0530

    Add files via upload

---
+++ cf_specifier.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 12;
+    printf("int value of a is %d",a);
+    printf("int value of b is %d",b);
+    
+
+    int c = 10;
+    int d = -12;
+    printf("int value of c is %u",c);
+    printf("int value of d is %u",d);
+    
+
+    int i = 0100;
+    printf("octal value of a is %o",i);
+    printf("interger value of i is %d",i);
+    
+    float j = 5.5;
+    printf("float value of %f",j);
+
+
+}
+
+
+
+    
