7292e41f42b20691dad991625470190d5a15b0be
Anjali167-prog <69163841+Anjali167-prog@users.noreply.github.com>
2023-09-25 18:36:36 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//this code displays how datatypes work in c
+
+
+/*this is the multiple line comment*/
+int main(){
+    int age = 17;
+    float pi = 3.14;
+    char star = '*'; 
+    printf("age is %d",age);
+    printf("value of pi is %f",pi);
+    printf("value of star is %c",star);
+    return 0 ;
+}
