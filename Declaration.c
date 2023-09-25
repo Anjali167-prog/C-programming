7270b048f8e55cfc069743ded46020c3899f6c37
Anjali167-prog <69163841+Anjali167-prog@users.noreply.github.com>
2023-09-25 18:37:07 +0530

    Add files via upload

---
+++ Declaration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a = 22;
+    int b = a ; 
+    printf("b is %d ",a );
+
+    int Age = 22;
+    int Years = 5;
+    int NewAge = Age + Years;
+    printf("NewAge is %d" , NewAge);
+
+    int x,y,z;
+     x = y = z = 1;
+    printf("z is %d" , z);
+    return 0 ;
+}
