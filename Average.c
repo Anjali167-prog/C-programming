40b4c0db83ed9488bc798c4edb18e29fb4363551
Anjali167-prog <69163841+Anjali167-prog@users.noreply.github.com>
2023-09-25 18:35:00 +0530

    Add files via upload

---
+++ Average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int x,y,z, result;
+    printf("Enter a first number");
+    scanf("%d",&x);
+    printf("Enter a second number");
+    scanf("%d",&y);
+    printf("Enter a Third number");
+    scanf("%d",&z);
+    result = (x + y + z )/3 ;
+    printf("%d",result );
+    return 0;
+
+}
